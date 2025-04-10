446f72978ed8e9a903d1d7244b93b51c1f407f29
Martin Grap <rmsk2@gmx.de>
2025-04-10 19:05:29 +0200

    Refactor

--- uidfactory.py
+++ uidfactory.py
@@ -24,12 +24,8 @@ class UidReaderRepo:
         self._atr_map = {}
         self.add_known_cards()
 
-    def add_known_cards(self):
-        self._atr_map[ATR_DES_FIRE] = desfire.DESFireUidReader(ATR_DES_FIRE)
-        self._atr_map[ATR_E_PERSO] = DummyReader(UidReaderRepo.get_default_id(ATR_E_PERSO), "German national ID", ATR_E_PERSO)
-        self._atr_map[ATR_GIRO] = DummyReader(UidReaderRepo.get_default_id(ATR_GIRO), "German Giro", ATR_GIRO)
-        self._atr_map[ATR_EGK] = DummyReader(UidReaderRepo.get_default_id(ATR_EGK), "German electronic health", ATR_EGK)
-
+        # automatically create a dummy uid reader for all cards which are
+        # not explicitly named
         all = set(ALL_ATRS)
         named = set(self._atr_map.keys())
         not_named = all - named        
@@ -37,6 +33,12 @@ class UidReaderRepo:
         for i in not_named:
             card_type = UidReaderRepo.get_default_id(i)
             self._atr_map[i] = DummyReader(card_type, f"Type {card_type}", i)
+
+    def add_known_cards(self):
+        self._atr_map[ATR_DES_FIRE] = desfire.DESFireUidReader(ATR_DES_FIRE)
+        self._atr_map[ATR_E_PERSO] = DummyReader(UidReaderRepo.get_default_id(ATR_E_PERSO), "German national ID", ATR_E_PERSO)
+        self._atr_map[ATR_GIRO] = DummyReader(UidReaderRepo.get_default_id(ATR_GIRO), "German Giro", ATR_GIRO)
+        self._atr_map[ATR_EGK] = DummyReader(UidReaderRepo.get_default_id(ATR_EGK), "German electronic health", ATR_EGK)
 
     @staticmethod
     def get_default_id(atr):
